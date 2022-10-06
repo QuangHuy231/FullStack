1f768ee08213452e436891336e86ad1a6094537a
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-10-06 22:52:39 +0700

    create table db

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -4,9 +4,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "nguyenquanghuya3kd@gmail.com",
+        password: "123456",
+        firstName: "Nguyen",
+        lastName: "Quang Huy",
+        address: "Binh Dinh",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
