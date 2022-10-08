346c1752df77ae16e423c97b41dfd0e0e3e39148
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-10-08 20:52:58 +0700

    Create new user CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,19 +1,29 @@
-
 import db from "../models/index";
-let getHomePage = async(req,res)=>{
-    try{
-        let data = await db.User.findAll();
-        
-        return res.render('homePage.ejs',{
-            data: JSON.stringify(data)
-        });
-    } catch (e) {
-        console.log(e)
-    }
-   
-   
-}
+import CRUDService from "../services/CRUDService";
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
 
+    return res.render("homePage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+let getCrud = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCrud = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  // console.log(req.body);
+  return res.send("post crud");
+};
 module.exports = {
-    getHomePage: getHomePage,
-}
+  getHomePage: getHomePage,
+  getCrud: getCrud,
+  postCrud: postCrud,
+};
--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,14 @@
 import express from "express";
-import homeController from "../controller/homeController"
+import homeController from "../controller/homeController";
 let router = express.Router();
 
-let initWebRoutes = (app)=>{
-    router.get('/',homeController.getHomePage);
-    router.get('/nguyenquanghuy',(req,res)=> res.send('hello hello'));
+let initWebRoutes = (app) => {
+  router.get("/", homeController.getHomePage);
+  router.get("/nguyenquanghuy", (req, res) => res.send("hello hello"));
+  router.get("/crud", homeController.getCrud);
+  router.post("/post-crud", homeController.postCrud);
 
-    return app.use("/",router);
-}
+  return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
